baadff5e78da6cccc9f008e3b753b72e70abc556
Ricardo Legorreta Mendoza <rlegorreta@scio.local>
2017-07-04 09:51:13 -0500

    First commit

---
+++ www/app/app.config.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+angular.module('Player')
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'PlayerCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+  $ionicConfigProvider.tabs.position('bottom');
+});
+
+})();
---
+++ www/app/app.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+     angular.module('Player', ['ionic', 'Player.controllers', 'Player.services']);
+
+})();
